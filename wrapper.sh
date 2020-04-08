1082e527e4ece669fc046c72263afb8273a10d00
Logan Cox <lo5an@users.noreply.github.com>
2020-04-08 15:24:03 -0500

    Document CAS startup.

--- wrapper.sh
+++ wrapper.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# Configure endpoints allowed to authenticate with CAS using file-based 
+# service registry. 
 if [ -n "${PRIVATE_GIT_SERVICE_REGISTRY+isset}" ] && [ -n "${BRANCH+isset}" ]
 then
   git clone -b ${BRANCH} ${PRIVATE_GIT_SERVICE_REGISTRY} /tmp/service-registry
 fi
 
+# Run CAS as a standalone web application using the embedded servlet container
+# Use server optimized JVM, and don't verify bytecode.  
 exec java -server -noverify -Xmx2048M "org.springframework.boot.loader.WarLauncher"
