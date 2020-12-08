6253a40c43704e2e53f1538b7eebf5ffd503b803
Jason Sherman <jsn.sherman@gmail.com>
2020-12-08 12:20:01 -0600

    add support for discovery feeds pulled from git.

--- wrapper.sh
+++ wrapper.sh
@@ -7,8 +7,14 @@ then
   git clone -b ${BRANCH} ${PRIVATE_GIT_SERVICE_REGISTRY} /tmp/service-registry
 fi
 
+# Configure delegated SAML2 authentication using file-based
+# discovery feeds.
+if [ -n "${PRIVATE_GIT_DISCO_FEEDS+isset}" ] && [ -n "${BRANCH+isset}" ]
+then
+  git clone -b ${BRANCH} ${PRIVATE_GIT_DISCO_FEEDS} /tmp/discovery-feeds
+fi
+
 # Run CAS as a standalone web application using the embedded servlet container
 # Use server optimized JVM, and don't verify bytecode.  
-
 
 exec java -server -noverify -Xmx2048M "org.springframework.boot.loader.WarLauncher"
